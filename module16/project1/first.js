76d3366a681f354881c279d0935b1c579806987f
Mr. Hasan <hasanshahriar32@gmail.com>
2022-07-31 20:01:32 +0600

    JS START

--- module16/project1/first.js
+++ module16/project1/first.js
@@ -2,9 +2,20 @@ console.log(5);
 console.log(36);
 
 /**
- *1. number
+ * 1. number
  * 2. int= 2,5
  * 3. float= 2.5
  * 4. string= "hello"
  * 5. operator= +, -, *, /, %
+ * 6. percent= %
+ * 7. average= (2+5)/2
+ * 8. comparison= >, <, >=, <=, ==, !=
+ * 9. oikik= 2 kilo apple and 1 kilo orange: 2*2+1*1
+ * 10. max, min, sum, difference, product, average
+ * 11. series= 1,2,3,4,5,6,7,8,9,10
+ * extra
+ * 1. prime number= 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97
+ * 2. permutation= 1,2,3,4,5,6,7,8,9,10
+ * ::::ALWAYS::::
+ * 
  * **/
