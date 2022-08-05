d4dd5e7f8eeddd44ad505ce98ae885f17ef81cd2
Mr. Hasan <hasanshahriar32@gmail.com>
2022-08-05 20:40:46 +0600

    js 2 start

---
+++ module17/tutorial/2.array.js
@@ -0,0 +1,5 @@
+var friendsAge = [20, 30, 40, 50];
+var picnicFee = [5000, 2000 , 1000, 500];
+var friendName = ["John", "Jane", "Bob", "Mary"];
+var friend = [friendsAge, picnicFee, friendName];
+console.log(friend);
---
+++ module17/tutorial/3.index.js
@@ -0,0 +1,13 @@
+var numbers = [12, 34, 345, 56];
+
+//get element by index
+var second = numbers [1];
+console.log(second);
+
+// find index of an element
+
+var position = numbers.indexOf(345);
+console.log(position);
+
+var lengths = numbers.length;
+console.log(lengths);
