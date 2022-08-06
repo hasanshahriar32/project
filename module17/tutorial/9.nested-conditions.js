48ae961b5b9fc94e3279a7183c72a71d1f8fbada
Mr. Hasan <hasanshahriar32@gmail.com>
2022-08-06 20:24:26 +0600

    module 17 done

---
+++ module17/tutorial/9.nested-conditions.js
@@ -0,0 +1,20 @@
+var math = true;
+var science = true;
+var history = true;
+geometry = true;
+var art = true;
+
+if (math ==true){
+    if (geometry == true){
+        console.log("You are a geometry major");
+    }
+    else{
+        console.log("You are a math major");
+    }
+
+
+}
+else{
+console.log('not good at math');
+
+}
