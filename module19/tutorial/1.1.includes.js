528fc18f7aa1eb9a1a65c501002caebc7d020392
Mr. Hasan <hasanshahriar32@gmail.com>
2022-08-09 15:54:43 +0600

    module 19 start

---
+++ module19/tutorial/1.1.includes.js
@@ -0,0 +1,4 @@
+// Example 2
+var name = "ariana grande is a singer";
+var word=  name.includes("is");
+console.log(word);
---
+++ module19/tutorial/4.return.js
@@ -0,0 +1,6 @@
+function add (num1, num2) {
+    // console.log(num1 + num2);
+    return num1 + num2;
+}
+var total = add(45,15);
+console.log(total);
