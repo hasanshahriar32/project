4b934cc510408616742bce7bb783363601f8fa47
Mr. Hasan <hasanshahriar32@gmail.com>
2022-08-14 19:29:19 +0600

    ok

---
+++ module23.5/tutorial/1.understand-recursion.js
@@ -0,0 +1,25 @@
+// let sum = 0;
+// for(let i=1; i<=5; i++){
+//     sum += i;
+
+// }
+// console.log(sum);
+
+
+// let sum = 0;
+// for (let i=5; i>=1; i--){
+//     sum += i;
+// }
+// console.log(sum);
+
+//recursion
+
+function sum(n){
+    if(n===1){
+        return 1;
+    }
+    else{
+        return 'false';
+    }
+}
+console.log(sum('tg'));
