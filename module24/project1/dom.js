93c36b988add41e70151386ec9e90f5c760d9827
Mr. Hasan <hasanshahriar32@gmail.com>
2022-08-17 16:55:56 +0600

    module 24 running

--- module24/project1/dom.js
+++ module24/project1/dom.js
@@ -1,3 +1,25 @@
-console.log(document);
 
+const student = {
+    name: 'nana',
+    id : 12,
+    study: function(time){
+        console.log(time,'study kortese');
+    }
+}
+
+const collection = document.getElementsByTagName('li');
+console.log(collection);
+for(const li of collection){
+    console.log(li.innerText);
+}
 document.getElementsByTagName('h1')
+
+
+const headings = document.getElementsByTagName('h1');
+console.log(headings);
+for (const h1 of headings){
+    console.log(h1.innerText);
+}
+
+let tour = document.getElementById('tour').innertext;
+console.log(tour);
