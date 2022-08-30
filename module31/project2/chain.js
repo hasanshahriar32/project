f1d3196db286541b6cd5f91e2722b52955ce1908
Mr. Hasan <hasanshahriar32@gmail.com>
2022-08-30 22:51:58 +0600

    module 31 start

---
+++ module31/project2/chain.js
@@ -0,0 +1,38 @@
+
+//object in a array
+const users = [
+    {
+        id: 1, name: 'John' , job : 'teacher'
+    }
+]
+
+console.log(users[0]);
+console.log(users[0].id);
+
+//array in a object
+const user = {
+    id: 1, name: 'John' , job : 'teacher',
+    friends: [
+        {id: 2, name: 'Mark'},
+        {id: 3, name: 'Jane'}
+    ]
+}
+console.log(user.friends[0].name);
+
+
+
+// Optional chaining at accessing nested elements
+
+// - Reason: In many cases, user don’t give required info(object element). if the program search anything inside this property, it will return error
+// - So, we use this to declare that: if there is an empty property/ no property then do not go further. Which will save us from getting error.
+
+const user1 = {
+    id: 1, name: 'John' , job : 'teacher',
+    friends: [
+        {id: 2, name: 'Mark', address: {city: 'New York', state: 'NY'}},
+        {id: 3, name: 'Jane'}
+    ]
+}
+console.log(user1.friends[0].name);
+console.log(user1.friends[0].address.city);
+console.log(user1.friends[1]?.address?.city);
