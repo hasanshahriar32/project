a14f3eb0276cfacddf01f2acff87f75c383bf409
Mr. Hasan <hasanshahriar32@gmail.com>
2022-09-08 09:58:06 +0600

    ok

--- module37/project1/async.js
+++ module37/project1/async.js
@@ -6,6 +6,13 @@ console.log('first');
 console.log('second');
 console.log('third');
 console.log('fourth');
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response =>{
+        console.log('api request completed');
+        return response.json();
+  })
+  //in case of multi-line arrow function, we need to use return keyword
+  .then(json => setTimeout(()=>{console.log(json)}, 3000));
 setTimeout(dosomething, 5000);
 setTimeout(()=>{console.log('lol')},2000);
 
