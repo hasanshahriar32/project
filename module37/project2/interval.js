c4c0f94f877c943722b8dd1e2a5338d0c2283241
Mr. Hasan <hasanshahriar32@gmail.com>
2022-09-08 11:58:01 +0600

    module 37 finishing

---
+++ module37/project2/interval.js
@@ -0,0 +1,17 @@
+console.log('first');
+console.log('second');
+
+
+// let seconds= 0;
+
+// const countSecond =  setInterval(()=> {
+//     console.log(++seconds);
+//     if (seconds === 10){
+//         clearInterval(countSecond);
+//     }
+
+// },1000)
+
+
+//timeout
+const thirdId = setTimeout(()=>{console.log('third');},5000)
