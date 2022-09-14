8e703fe4fc3795153f5c64707523796da66e8ece
Mr. Hasan <hasanshahriar32@gmail.com>
2022-09-14 19:25:43 +0600

    module 38 done 39 start

---
+++ module39/project4/triple.js
@@ -0,0 +1,34 @@
+// == checks only value
+
+const first = 2;
+const second = 2;
+if (first == second){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+// != checks only value
+// !== checks value and type
+
+
+// === checks value and type
+const thied = 2;
+const fourth = '2';
+if (thied === fourth){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+//type coercion/type conversion/ type casting 
+//converts one type to another type
+//converts string to number
+
+const first1 = 2;
+const second1 = '2';
+const sum = first1 + second1;
+console.log(sum);
+
