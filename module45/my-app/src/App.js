a357c45a97fb19123fb36a9f33a12ac0de722fb7
Mr. Hasan <hasanshahriar32@gmail.com>
2022-09-23 02:46:05 +0600

    ok

--- module45/my-app/src/App.js
+++ module45/my-app/src/App.js
@@ -3,43 +3,33 @@ import './App.css';
 
 
 
-const version = 0.1;
-const about = {
-  name: 'React',
-  version: 17.0
-}
+// const version = 0.1;
+// const about = {
+//   name: 'React',
+//   version: 17.0
+// }
 
 
-const aboutStyle = {
-  color: 'red',
-  fontSize: '20px'
-}
+// const aboutStyle = {
+//   color: 'red',
+//   fontSize: '20px'
+// }
 
 function App() {
   return (
+
+ 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>Hello react!! <span>{version}</span></h3>
-          <p style={aboutStyle}>{about.name} version: {about.version}</p>
-        </div>
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name="anon" job="cyber specialist"></Person>
+      <Person name = "fuad"></Person>
+      <Person name = "kajol" job = "coding"></Person>
     </div>
   );
+}
+
+function Person( props ) {
+  return  <div className='person'><h1>{props.name}</h1>
+            <p>{props.job}</p></div>
 }
 
 export default App;
