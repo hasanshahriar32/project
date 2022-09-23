89b9c4ba9045aef3d5a36659ad09e0f0377154b2
Mr. Hasan <hasanshahriar32@gmail.com>
2022-09-23 17:10:13 +0600

    react start project 1

--- module45/my-app/src/App.js
+++ module45/my-app/src/App.js
@@ -16,20 +16,30 @@ import './App.css';
 // }
 
 function App() {
+  // const name = ['anon', 'anon2', 'anon3'];	
+  const name2 = [{id: 1, name: 'anon', job: 'developer', addres: 'USA'}, 
+                 {id: 2, name: 'anon2', job: 'developer', addres: 'USA'},
+                 {id: 3, name: 'anon3', job: 'developer', addres: 'USA'},
+                 {id: 4, name: 'anon4', job: 'developer', addres: 'USA'},];
   return (
-
+    
  
     <div className="App">
-      <Person name="anon" job="cyber specialist"></Person>
+      {
+      // name.map(name => <Person name={name}></Person>)
+      name2.map(name => <Person serial={name.id} name={name.name} job={name.job} addres={name.addres}></Person>)
+    }
+      {/* <Person name="anon" addres={addres[0]} job="cyber specialist"></Person>
       <Person name = "fuad"></Person>
-      <Person name = "kajol" job = "coding"></Person>
+      <Person name = "kajol" job = "coding"></Person> */}
     </div>
   );
 }
 
 function Person( props ) {
-  return  <div className='person'><h1>{props.name}</h1>
-            <p>{props.job}</p></div>
+  return  <div className='person'><h1><span>id{props.serial}: </span>{props.name}</h1>
+            <p>{props.job}</p>
+            <p>{props.addres}</p></div>
 }
 
 export default App;
