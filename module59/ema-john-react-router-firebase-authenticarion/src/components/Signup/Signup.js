af064a7f623cee15469c3cdad6f35c50a2ee18e8
Mr. Hasan <hasanshahriar32@gmail.com>
2022-10-28 18:21:06 +0600

    module 59 going

--- module59/ema-john-react-router-firebase-authenticarion/src/components/Signup/Signup.js
+++ module59/ema-john-react-router-firebase-authenticarion/src/components/Signup/Signup.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 const Signup = () => {
+    const [error,setError] = useState('')
+    const handleSubmit =(e)=>{
+
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirm = form.confirm.value;
+        const name = form.name.value;
+        const image = form.image.value;
+        console.log(email, password, confirm);
+        if(password!=confirm){
+            setError('password mismatch!!')
+            return;
+        }
+        setError('');
+        
+
+
+    }
     return (
         <div className='form-container'>
             <h1>Signup</h1>
-            <form action="">
+            <form onSubmit={handleSubmit} action="">
                 <input type="text" name="name" id="" placeholder='Your Name' />
                 <br />
                 <input type="text" name="image" id="" placeholder='Your Image Link' />
@@ -18,6 +38,8 @@ const Signup = () => {
                 <button type="submit">Register</button>
                 <br />
                 <small>Already a member? <Link to='/login'>login</Link> </small>
+                <br />
+                <small >{error}</small>
             </form>
         </div>
     );
